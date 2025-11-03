3be1c321d84f6dab743dfc1af53535dbcd0a2a7e
Harsha Vardhan <harshachundru7@gmail.com>
2025-11-03 17:00:05 +0530

    Removed target folder and added .gitignore

--- OpencartV121/src/test/java/testBase/TestBaseClass.java
+++ OpencartV121/src/test/java/testBase/TestBaseClass.java
@@ -18,6 +18,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
